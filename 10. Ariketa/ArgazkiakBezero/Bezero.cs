e72c12dd6e07f063286d7a8b37a50a35ed8367e4
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-11-13 12:19:07 +0100

    Argazkiak

---
+++ 10. Ariketa/ArgazkiakBezero/Bezero.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+class Bezero
+{
+    const string SERVER = "127.0.0.1";
+    const int PORT = 5000;
+    private static readonly string? ImgDir = 
+        Directory.GetParent(Environment.CurrentDirectory)?
+        .Parent?
+        .Parent?
+        .Parent?
+        .FullName + "\\";
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("BEZEROA hasi da");
+
+        Console.Write("Sartu zure izena: ");
+        string izena = Console.ReadLine();
+        string userPath = $"{ImgDir}{izena}_irudiak";
+        if (!Directory.Exists(userPath))
+        {
+            Directory.CreateDirectory(userPath);
+            Console.WriteLine($"""
+                Carpeta berri bat sortu da '{izena}'-(r)entzat:
+                {userPath}
+                """);
+        }
+        else Console.WriteLine($"Ongi entorri {izena}");
+
+        try
+        {
+            using TcpClient client = new();
+            client.Connect(SERVER, PORT);
+            using NetworkStream ns = client.GetStream();
+            using StreamReader reader = new(ns, Encoding.UTF8);
+            using StreamWriter writer = new(ns, Encoding.UTF8);
+            {
+                writer.AutoFlush = true;
+                byte[] bytes = new byte[client.ReceiveBufferSize];
+
+                for (int i = 0; i < 5; i++)
+                    Console.WriteLine(reader.ReadLine());
+
+                int result = 0;
+                while (result != 4)
+                {
+                    Console.Write("Aukera: ");
+                    if (Int32.TryParse(Console.ReadLine(), out result))
+                    {
+                        writer.WriteLine(result);
+                        Console.WriteLine(reader.ReadLine());
+
+                        byte[] imgBytes = new byte[Int32.Parse(reader.ReadLine())];
+
+                        int newBytes = -1;
+                        for (int i = 0; i < imgBytes.Length && newBytes != 0; i += newBytes)
+                            newBytes = ns.Read(imgBytes, i, imgBytes.Length - i);
+
+                        string fileName = reader.ReadLine();
+                        File.WriteAllBytes(userPath + "\\" + fileName, imgBytes);
+                        Console.WriteLine("Image download: " + userPath + "\\" + fileName);
+                    }
+                    else Console.WriteLine("Error: formatu ezegokia");
+                }
+
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+        catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+        Console.WriteLine("Konexioa amaitu da");
+    }
+}
---
+++ 10. Ariketa/ArgazkiakZerbitzari/Zerbitzari.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Zerbitzari
+{
+    const int PORT = 5000;
+    const string IMG_DIR = "D:\\PSP\\Ariketak\\10. Ariketa\\ImgZerbitzari";
+    private static readonly List<Thread> Connections = new(5);
+    private static readonly Dictionary<int, string> Images = [];
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("ZERBITZARIA hasi da");
+        Images.Add(1, "mendi_argazkia.jpg");
+        Images.Add(2, "hondartza_argazkia.jpg");
+        Images.Add(3, "hiri_argazkia.jpg");
+
+        try
+        {
+            using TcpListener listener = new(IPAddress.Any, PORT);
+            {
+                listener.Start();
+                Console.WriteLine("Zerbitzaria entzuten dago: port " + PORT);
+
+                while(true)
+                    if (Connections.Count < Connections.Capacity)
+                        Connections.Add(NewConnection(listener.AcceptTcpClient()));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Server error: " + e.ToString());
+        }
+
+        Console.WriteLine("ZERBITZARIA itxi da");
+    }
+
+    private static Thread NewConnection(TcpClient socket)
+    {
+        Thread connection = new(_ =>
+        {
+            string izena = "";
+            try
+            {
+                using NetworkStream ns = socket.GetStream();
+                using StreamReader reader = new(ns, Encoding.UTF8);
+                using StreamWriter writer = new(ns, Encoding.UTF8);
+                {
+                    writer.AutoFlush = true;
+                    string name = socket.Client.RemoteEndPoint.ToString();
+                    Console.WriteLine($"Bezero berri bat konektatu da: {name}");
+                    writer.WriteLine("""
+                        Ongi etorri argazki zerbitzarira, aukeratu:
+                        1 - Mendi argazkia
+                        2 - Hondartza argazkia
+                        3 - Hiri argazkia
+                        4 - Irten
+                        """);
+
+                    int emaitza = 0;
+                    while (emaitza != 4)
+                    {
+                        if(Int32.TryParse(reader.ReadLine(), out emaitza) && emaitza != 4)
+                        {
+                            Console.WriteLine($"{name}: {emaitza}");
+                            writer.WriteLine($"'{Images[emaitza]}' aukeratu duzu");
+
+                            byte[] imgBytes = File.ReadAllBytes(IMG_DIR +"\\"+ Images[emaitza]);
+                            writer.WriteLine(imgBytes.Length);
+                            ns.Write(imgBytes, 0, imgBytes.Length);
+
+                            writer.WriteLine(Images[emaitza]);
+                            Console.WriteLine($"'{Images[emaitza]}' irudia bidalita");
+                        }
+                    }
+
+                    writer.WriteLine("Agur");
+                    Console.WriteLine($"Bezero {izena} deskonektatu da");
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Client error: {izena} disconnected");
+            }
+            finally { socket.Close(); }
+        });
+        connection.Start();
+        return connection;
+    }
+}
